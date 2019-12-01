5a7cb25b0338516dfe53b8fbcd3cb03599edde8f
Dexodus <daniilarosuk9@gmail.com>
2019-12-01 22:43:53 +0300

    oops

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=app',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=mysql;dbname=yii_auth',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
